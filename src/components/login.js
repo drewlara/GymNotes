b5c88cae136f59973b39b088557b9a7f655d9ba3
Andrew Lara <drewlata@gmail.com>
2018-11-30 13:28:44 -0600

    add demo info to login page

--- src/components/login.js
+++ src/components/login.js
@@ -4,15 +4,40 @@ import { connect } from 'react-redux';
 import LoginForm from './login-form';
 import './login.css';
 
-export function LoginPage(props) {
-  if (props.loggedIn) {
-    return <Redirect to="/dashboard" />
+export class LoginPage extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      isHidden: true
+    }
   }
-  return (
-    <div className="login-page">
-      <LoginForm />
-    </div>
-  );
+
+  render() {
+    if (this.props.loggedIn) {
+      return <Redirect to="/dashboard" />
+    }
+    let demoUserInfo;
+
+    if (!this.state.isHidden) {
+      demoUserInfo = (
+        <div className='demo-user-info'>
+          <p>Username: demouser</p>
+          <p>Password: demouserpassword</p>
+        </div>
+      )
+    }
+    
+
+    return (
+      <div className="login-page">
+        <LoginForm />
+        <div className="demo">
+          <p>Want to demo GymNotes? <span className="demo-show" onClick={() => this.setState({isHidden: false})}>Click Here!</span></p>
+          {demoUserInfo}  
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => ({
